baccc3371eeeee94f3fcde0fbbd5a2f88c62b5d3
majid zamanian <90752400+majidzamanian@users.noreply.github.com>
2023-01-03 21:37:23 +0330

    new easy game

---
+++ rock_paper_scissors.py
@@ -0,0 +1,18 @@
+import random
+
+box = ["rock", "paper", "scissors"]
+y = random.choice(box)
+choose = str(input("lets play Rock, paper, scissors. we only have 1 rounds. lets go. what is your weapon? Rock, paper or scissors? "))
+
+if choose == y:
+    print("tie!")
+elif choose == ("paper") and y == ("rock"):
+    print(f"you with {choose} won agenst computyer {y}" )
+elif choose == ("paper") and y == ("scissors"):
+    print(f"you with {choose} lost agenst computyer {y}" )
+elif choose == ("rock") and y == ("scissors"):
+    print(f"you with {choose} won agenst computyer {y}" )
+
+
+
+
