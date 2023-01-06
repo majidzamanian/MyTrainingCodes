6bd305d6e4840e5ceee664f541b1961c6811f1f7
majid zamanian <90752400+majidzamanian@users.noreply.github.com>
2023-01-06 20:59:56 +0330

    lower and upper case handle

--- Hangman.py
+++ Hangman.py
@@ -2,22 +2,21 @@
 import random
 
 #the words we want to find in this game
-List_Of_Words = ["murderers", "rapists", "thiefs"]
+List_Of_Words = ["Navid Afkari", "Shahram Amiri", "Ayaz Marhoni","Mahmoud Asgari","Mahmoud Asgari","Mosleh Zamani","Hossein Soodmand","mahsa amini"]
 
 #selecting a word from list by computer
-word = random.choice(List_Of_Words)
-print(word)
+word = random.choice(List_Of_Words).lower()
 count = len(word)
 
 #making a list for gussing by user by using the word choosed from list
 list_of_the_word = ["-"] *  count
 
 # the amount of man life
-i=3
+i=5
 while count!=0:
     # input a character by user
-    choice = input("what is your character?")
-    if not choice.isalpha() or len(choice) > 1 or word.find(choice) <0:
+    choice = input("what is your character?").lower()
+    if not choice.isalpha() or len(choice) > 1 or word.find(choice) <0 :
         #lifes
         print(f"you have only {i} life left")
         i-=1
