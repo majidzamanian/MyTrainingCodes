f686f3d4bd5b2142017ceaf732224fbc781b0f45
majid zamanian <90752400+majidzamanian@users.noreply.github.com>
2023-01-09 21:33:07 +0330

    final edit

--- password generator.py
+++ password generator.py
@@ -1,11 +1,31 @@
 import string
 import random
+
+#defining the passwrd materials
 keys = []
 s = string.printable
 for i in s :
     keys.append(i)
 
-lengh= int(input("how long do u want your password be? " ))
+#the main questions
 
-for g in range (0,lengh):
-    print(random.choice(keys),end = '')
+
+s = "yes"
+
+#ask user if its over or not
+
+def generator():
+    lengh = int(input("how long do u want your password be? "))
+    for g in range(0, lengh):
+        print(random.choice(keys), end='')
+
+
+
+while s == "yes":
+    generator()
+    if input("\nwanna another password? ") == "no":
+        input("\nPress enter to close program")
+        break
+
+
+
